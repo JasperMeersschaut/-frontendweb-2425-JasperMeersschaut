0e58a7e38b3200bc339d559cfc0e6135bf24277a
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-11-15 16:58:36 +0100

    feat: changed database error handling

--- gymstats-webservice/src/service/_handleDBError.ts
+++ gymstats-webservice/src/service/_handleDBError.ts
@@ -1,15 +1,10 @@
-// src/service/_handleDBError.ts
-import ServiceError from '../core/serviceError'; 
-//TODO: nog allemaal aanpassen naar mate van mijn databank
+import ServiceError from '../core/serviceError';
+
 const handleDBError = (error: any) => {
   const { code = '', message } = error;
 
   if (code === 'P2002') {
     switch (true) {
-      case message.includes('idx_place_name_unique'):
-        throw ServiceError.validationFailed(
-          'A place with this name already exists',
-        );
       case message.includes('idx_user_email_unique'):
         throw ServiceError.validationFailed(
           'There is already a user with this email address',
@@ -21,28 +16,30 @@ const handleDBError = (error: any) => {
 
   if (code === 'P2025') {
     switch (true) {
-      case message.includes('fk_transaction_user'):
+      case message.includes('fk_userWorkout_user'):
         throw ServiceError.notFound('This user does not exist');
-      case message.includes('fk_transaction_place'):
-        throw ServiceError.notFound('This place does not exist');
-      case message.includes('transaction'):
-        throw ServiceError.notFound('No transaction with this id exists');
-      case message.includes('place'):
-        throw ServiceError.notFound('No place with this id exists');
+      case message.includes('fk_userWorkout_workout'):
+        throw ServiceError.notFound('This workout does not exist');
+      case message.includes('userWorkout'):
+        throw ServiceError.notFound('No user workout with this id exists');
+      case message.includes('workout'):
+        throw ServiceError.notFound('No workout with this id exists');
       case message.includes('user'):
         throw ServiceError.notFound('No user with this id exists');
+      case message.includes('exercise'):
+        throw ServiceError.notFound('No exercise with this id exists');
     }
   }
 
   if (code === 'P2003') {
     switch (true) {
-      case message.includes('place_id'):
-        throw ServiceError.conflict(
-          'This place does not exist or is still linked to transactions',
-        );
       case message.includes('user_id'):
         throw ServiceError.conflict(
-          'This user does not exist or is still linked to transactions',
+          'This user does not exist or is still linked to workouts',
+        );
+      case message.includes('workout_id'):
+        throw ServiceError.conflict(
+          'This workout does not exist or is still linked to users',
         );
     }
   }
