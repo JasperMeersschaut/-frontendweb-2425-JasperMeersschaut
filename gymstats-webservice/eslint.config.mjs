8c242a28916695ee5e67d1795d3ae4343384ef1f
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-12-05 13:52:29 +0100

    fix: update ESLint configuration

--- gymstats-webservice/eslint.config.mjs
+++ gymstats-webservice/eslint.config.mjs
@@ -1,48 +1,40 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin';
 import jest from 'eslint-plugin-jest';
-import pluginCypress from 'eslint-plugin-cypress/flat';
 
 export default tseslint.config(
-  eslint.configs.recommended, 
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginCypress.configs.recommended,
+  { ignores: ['build'] },
   {
-    // 👇 3
     files: ['**/*.ts', '**/*.spec.ts'],
     plugins: {
       '@stylistic': stylistic,
     },
     rules: {
-      '@stylistic/no-multiple-empty-lines': [
-        'error',
-        {
-          max: 1,
-          maxEOF: 1,
-          maxBOF: 0,
-        },
-      ],
-      '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+      '@stylistic/no-multiple-empty-lines': ['error', {
+        max: 1, maxEOF: 1, maxBOF: 0,
+      }],
+      '@stylistic/indent': ['error', 2, { 'SwitchCase': 1 }],
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/semi': ['error', 'always'],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@stylistic/no-tabs': ['error'],
-      '@stylistic/max-len': [
-        'error',
-        {
-          code: 120,
-          tabWidth: 2,
-        },
-      ],
+      '@stylistic/max-len': ['error', {
+        'code': 120,
+        'tabWidth': 2,
+      }],
       '@stylistic/arrow-parens': ['error', 'always'],
-      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      '@stylistic/brace-style': ['error', '1tbs', { 'allowSingleLine': false }],
       '@stylistic/no-inner-declarations': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
     },
+  },
+  {
+    files: ['**/*.spec.ts'],
     plugins: { jest },
   },
 );
