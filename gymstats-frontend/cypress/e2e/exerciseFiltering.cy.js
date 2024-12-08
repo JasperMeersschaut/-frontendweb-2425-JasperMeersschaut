2ff5bc050dd7e6f36a014878b0bf56d27323ceb9
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-12-08 15:39:13 +0100

    feat: add exercise filtering tests and mock data for Cypress

--- gymstats-frontend/cypress/e2e/exerciseFiltering.cy.js
+++ gymstats-frontend/cypress/e2e/exerciseFiltering.cy.js
@@ -4,11 +4,17 @@ describe('Exercise Filtering', () => {
   });
   
   it('Filters to arms only', () => {
+    cy.intercept(
+      'GET',
+      'http://localhost:9000/api/exercises',
+      { fixture: 'exercises.json' }, 
+    );
+    cy.visit('http://localhost:5173');
     cy.get('[data-cy=exercises_btn]').click();
     cy.url().should('include', '/exercises');
       
     cy.contains('[data-cy=muscle_group_label]', 'Arms').click();
-      
+    cy.get('[data-cy^=exercise_card_]').should('have.length', 8);
     cy.get('[data-cy=exercise_list]').within(() => {
       cy.get('[data-cy=exercise_muscle_group]').each((titel) => {
         cy.wrap(titel).should('contain', 'Arms');
