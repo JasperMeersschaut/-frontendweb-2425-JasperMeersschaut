1e1cecdfef12cdc263c8113f2895f3d43430c043
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-12-09 20:33:30 +0100

    refactor: responsive WorkoutCard and WorkoutList components

--- gymstats-frontend/src/components/workouts/WorkoutCard.jsx
+++ gymstats-frontend/src/components/workouts/WorkoutCard.jsx
@@ -4,8 +4,7 @@ import { axios } from '../../api/index.js';
 
 const contentURL = axios.defaults.contentURL;
 
-export default function WorkoutCard({ workout, onDelete,user, homepage }) {
-
+export default function WorkoutCard({ workout, onDelete, user, homepage }) {
   const currentUserId = user.id;
   const currentUserRoles = user.roles;
 
@@ -17,13 +16,14 @@ export default function WorkoutCard({ workout, onDelete,user, homepage }) {
     return array;
   };
 
-  const shuffledExercises = shuffleArray([...workout.items]).slice(0, 4); // Neemt elke keer een willekeurige image
+  const shuffledExercises = shuffleArray([...workout.items]).slice(0, 4); // Ensure only 4 images are displayed
 
   const handleDelete = () => {
     if (onDelete) {
       onDelete(workout.id);
     }
   };
+
   const canEdit = currentUserId === workout.createdBy || currentUserRoles.includes('admin');
 
   return (
@@ -33,12 +33,12 @@ export default function WorkoutCard({ workout, onDelete,user, homepage }) {
           <h5 className="text-xl font-bold mb-2">{workout.type}</h5>
           <p className="text-gray-700 mb-2">Duration: {workout.duration} minutes</p>
           <p className="text-gray-700 mb-4">Muscle Focus: {workout.muscleFocus}</p>
-          <div className="flex flex-wrap justify-center gap-4">
+          <div className="grid grid-cols-2 gap-4">
             {shuffledExercises.map((exercise) => (
               <img
                 key={exercise.id}
                 src={`${contentURL}/images/exercises/${exercise.id}.jpg`}
-                className="w-24 h-24 object-contain border border-black"
+                className="w-full h-24 object-contain border border-black"
                 alt={exercise.type}
                 onError={(e) => (e.target.src = `${contentURL}/images/exercises/default.jpg`)}
               />
@@ -46,7 +46,7 @@ export default function WorkoutCard({ workout, onDelete,user, homepage }) {
           </div>
         </div>
       </Link>
-      {canEdit &&!homepage && (
+      {canEdit && !homepage && (
         <div className="flex justify-end mt-4">
           <Link to={`/workouts/edit/${workout.id}`} className="btn btn-light" data-cy="workout_edit_btn">
             <IoPencilOutline />
