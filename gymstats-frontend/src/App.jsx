1827a5906c4aeb9d578ac6daecde19eee7b8b24d
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-12-09 20:35:03 +0100

    feat: responsive App component for mobile

--- gymstats-frontend/src/App.jsx
+++ gymstats-frontend/src/App.jsx
@@ -32,42 +32,59 @@ function App() {
   const handleImageError = (e) => {
     e.target.src = `${contentURL}/images/profilePictures/0.jpg`;
   };
-  
-  return(
-    <div className="flex">
-      <div className="fixed left-0 top-0 h-full w-1/5 p-4 bg-white shadow-lg overflow-y-auto my-16">
-        <h1 className="text-2xl font-bold mb-4">Workouts</h1>
-        <div className="flex flex-col gap-6">
-          <AsyncData loading={workoutsLoading || userLoading} error={workoutError || userError}>
-            {workouts && workouts.map((workout) => (
-              <WorkoutCard key={workout.id} workout={workout} user={user} homepage={true}  />
-            ))}
+
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row pt-1">
+        <div className="hidden lg:block lg:fixed left-0 top-0 lg:h-full w-full lg:w-1/5 p-4 
+        bg-white shadow-lg overflow-y-auto my-16 lg:my-0 pt-16">
+          <h1 className="text-2xl font-bold mb-4 pt-8 text-center">Workouts</h1>
+          <div className="flex flex-col gap-6">
+            <AsyncData loading={workoutsLoading || userLoading} error={workoutError || userError}>
+              {workouts && workouts.map((workout) => (
+                <WorkoutCard key={workout.id} workout={workout} user={user} homepage={true} />
+              ))}
+            </AsyncData>
+          </div>
+        </div>
+        <div className="lg:col-span-3 p-4 mx-40">
+          <h1 className="text-3xl font-bold mb-4 text-center">Exercises</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <AsyncData loading={exercisesLoading || userLoading} error={exercisesError || userError}>
+              {exercises && exercises.map((exercise) => (
+                <div key={exercise.id} className="p-2">
+                  <ExerciseCard exercise={exercise} currentUserRoles={user.roles} homepage={true} />
+                </div>
+              ))}
+            </AsyncData>
+          </div>
+        </div>
+        <div className="block lg:hidden fixed right-0 top-0 h-full w-full lg:w-1/5 flex items-center justify-center p-4 
+        bg-white shadow-lg mt-16 lg:mt-0">
+          <AsyncData loading={userLoading} error={userError}>
+            {user && (
+              <ProfileComponent
+                user={user}
+                bmiData={bmiData}
+                contentURL={contentURL}
+                handleImageError={handleImageError}
+              />
+            )}
           </AsyncData>
         </div>
-      </div>
-      <div className="flex-grow p-4 mx-40">
-        <h1 className="text-3xl font-bold mb-4">Exercises</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          <AsyncData loading={exercisesLoading || userLoading} error={exercisesError || userError}>
-            {exercises && exercises.map((exercise) => (
-              <div key={exercise.id} className="p-2">
-                <ExerciseCard exercise={exercise} currentUserRoles={user.roles} homepage={true} />
-              </div>
-            ))}
+        <div className="hidden lg:block fixed right-0 top-0 h-full w-1/5 flex items-center justify-center p-4 
+        bg-white shadow-lg pt-16">
+          <AsyncData loading={userLoading} error={userError}>
+            {user && (
+              <ProfileComponent
+                user={user}
+                bmiData={bmiData}
+                contentURL={contentURL}
+                handleImageError={handleImageError}
+              />
+            )}
           </AsyncData>
         </div>
-      </div>
-      <div className="fixed right-0 top-0 h-full w-1/5 flex items-center justify-center p-4 bg-white shadow-lg">
-        <AsyncData loading={userLoading} error={userError}>
-          {user && (
-            <ProfileComponent
-              user={user}
-              bmiData={bmiData}
-              contentURL={contentURL}
-              handleImageError={handleImageError}
-            />
-          )}
-        </AsyncData>
       </div>
     </div>
   );
