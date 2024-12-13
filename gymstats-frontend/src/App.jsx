fbc99bbb0ae76fb4712680d121041082f88cb6ad
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-12-13 15:28:59 +0100

    fix: changed BMI data fetching to use backend

--- gymstats-frontend/src/App.jsx
+++ gymstats-frontend/src/App.jsx
@@ -1,34 +1,19 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import { axios } from './api/index.js';
 import useSWR from 'swr';
 import AsyncData from './components/AsyncData.jsx';
 import ProfileComponent from './components/Profile/ProfileComponent.jsx';
 import WorkoutCard from './components/workouts/WorkoutCard.jsx';
 import ExerciseCard from './components/exercises/ExerciseCard.jsx';
-import { getById,getAll } from './api/index.js';
+import { getById, getAll } from './api/index.js';
 
 function App() {
   const contentURL = axios.defaults.contentURL;
-  const [bmiData, setBmiData] = useState(null);
   const { data: user, isLoading: userLoading, error: userError } = useSWR('users/me', getById);
   const { data: workouts = [], isLoading: workoutsLoading, error: workoutError } = useSWR('workouts', getAll);
   const { data: exercises = [], isLoading: exercisesLoading, error: exercisesError } = useSWR('exercises', getAll);
+  const { data: bmiData, isLoading: bmiLoading, error: bmiError } = useSWR('bmi', getById);
 
-  useEffect(() => {
-    const fetchBmi = async () => {
-      try {
-        const { data } = await axios.get(`/bmi/${user.id}`);
-        setBmiData(data);
-      } catch (error) {
-        console.error('Error fetching BMI data:', error);
-      }
-    };
-
-    if (user) {
-      fetchBmi();
-    }
-  }, [user]);
   const handleImageError = (e) => {
     e.target.src = `${contentURL}/images/profilePictures/0.jpg`;
   };
@@ -61,7 +46,7 @@ function App() {
         </div>
         <div className="block lg:hidden fixed right-0 top-0 h-full w-full lg:w-1/5 flex items-center justify-center p-4 
         bg-white shadow-lg mt-16 lg:mt-0">
-          <AsyncData loading={userLoading} error={userError}>
+          <AsyncData loading={userLoading || bmiLoading} error={userError || bmiError}>
             {user && (
               <ProfileComponent
                 user={user}
@@ -74,7 +59,7 @@ function App() {
         </div>
         <div className="hidden lg:block fixed right-0 top-0 h-full w-1/5 flex items-center justify-center p-4 
         bg-white shadow-lg pt-16">
-          <AsyncData loading={userLoading} error={userError}>
+          <AsyncData loading={userLoading || bmiLoading} error={userError || bmiError}>
             {user && (
               <ProfileComponent
                 user={user}
